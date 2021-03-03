33f2557931708bc48b8e4af8b0a54f05af8b784a
Leandro Nunes <nunes.pessoal@gmail.com>
2021-03-02 21:34:16 -0300

    Exercise finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useForm } from "react-hook-form";
 
 function App() {
+  const { register, handleSubmit, watch, errors } = useForm();
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
+  const watchEstadoCivil = watch('estado_civil')
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input name="nome" ref={register({ required:true })} placeholder="Nome"/>
+        {errors.nome && " *Nome obrigatório"}
+        <br/>
+        <input name="cidade" ref={register} placeholder="Cidade"/>
+        <br/>
+        <input name="email" ref={register({ required:true, pattern: /^\S+@\S+$/i })} placeholder="E-mail" />
+        {errors.email && " *Informe um e-mail válido"}
+        <br/>
+        <input name="idade" type="number" ref={register({ required:true, min: 18, max: 200})} placeholder="Idade"/>
+        {errors.idade && " *Idade mínima: 18 anos"}
+        <br/>
+        <label>Estado Civil:</label><br></br>
+        <label>Solteiro</label>
+        <input name="estado_civil" type="radio" ref={register({ required:true })} value="solteiro"/>
+
+        <label>Casado</label>
+        <input name="estado_civil" type="radio" ref={register({ required:true })} value="casado"/>
+        {watchEstadoCivil === "casado" && (
+          <div>
+            <label>Nome do conjuge:</label>
+            <input name="conjuge" type="text" ref={register({ required:true })}/>
+            {errors.conjuge && " *Informe o nome do seu conjuge"}
+          </div>
+        )}
+        {errors.estado_civil && " *Informe seu estado civil"}
+        <br></br>        
+
+        <button>Enviar</button>
+
+      </form>
+      
     </div>
   );
 }
